1668eb7a77bb316f2cabcbe14129b17dab3fc44b
Tanuj Sharma <ta904513@gmail.com>
2026-03-27 11:28:58 +0530

    [Tanuj Sharma]Refactored. User based signup and login jwt auth

--- src/main/java/com/app/quantitymeasurement/controller/AuthController.java
+++ src/main/java/com/app/quantitymeasurement/controller/AuthController.java
@@ -1,21 +1,47 @@
 package com.app.quantitymeasurement.controller;
 
+import com.app.quantitymeasurement.dto.request.AuthRequest;
+import com.app.quantitymeasurement.entity.User;
+import com.app.quantitymeasurement.repository.UserRepository;
 import com.app.quantitymeasurement.security.JwtUtil;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
-    public AuthController(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
+    @PostMapping("/signup")
+    public String signup(@RequestBody AuthRequest request) {
+
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role("USER")
+                .build();
+
+        userRepository.save(user);
+
+        return "User registered successfully";
     }
 
-    @PostMapping("/login/{username}")
-    public String login(@PathVariable String username) {
-        return jwtUtil.generateToken(username);
-    }
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest request) {
 
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return jwtUtil.generateToken(user.getUsername());
+    }
 }
